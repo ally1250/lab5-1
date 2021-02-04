b97a24017d1f434a3bfcbd73a1e5a6a670a9f66e
Wang, Danling <daw001@ucsd.edu>
2021-02-04 14:06:00 -0800

    add check when add friend

--- routes/add.js
+++ routes/add.js
@@ -1,21 +1,23 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response) {
+
 	// form variables:
 	const {
 		name,
 		description,
 	} = request.query;
 
-	const newFriend = {
-		name,
-		description,
-		imageURL: "http://lorempixel.com/400/400/people",
-	};
+	// if (name.length > 0 && description.length > 0) {
+		const newFriend = {
+			name,
+			description,
+			imageURL: "http://lorempixel.com/400/400/people",
+		};
 
-	data.friends.push(newFriend);
+		data.friends.push(newFriend);
 
-	// console.log({newFriend});
-	response.render('index', data);
+		response.render('index', data);
+	// }
 	// Your code goes here
 }
